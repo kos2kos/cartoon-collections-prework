4d4e22a0ec58c9025bc18d1baee5a36cc7f73ac4
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 19:18:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |call, index|
-    puts "\n" + call
+    puts  "#{index}. "+ call
   end
 end
 
